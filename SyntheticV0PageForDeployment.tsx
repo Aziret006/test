c838df621f16abca849e498318d81c3160276c3f
Азирет <aziretaalybaev2006@gmail.com>
2025-04-22 13:33:32 +0600

    fix

--- SyntheticV0PageForDeployment.tsx
+++ SyntheticV0PageForDeployment.tsx
@@ -1,8 +1,7 @@
 "use client"
-
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import "../test/styles/promotions.scss" 
+import "./styles/promotions.scss" 
 
 interface Promotion {
   id: number
